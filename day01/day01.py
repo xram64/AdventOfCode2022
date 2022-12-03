7a1ee42dff93000987e52f060bc22fd641691c9b
xram64 <33797815+xram64@users.noreply.github.com>
2022-12-02 20:10:11 -0500

    Update title formatting

--- day01/day01.py
+++ day01/day01.py
@@ -1,12 +1,24 @@
-## Advent of Code 2022: Day 1
-## https://adventofcode.com/2022/day/1
-## Jesse Williams | github.com/xram64
-## Answers: [Part 1]: 69310, [Part 2]: 206104
+#│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│#
+#   ______________________________________________________________   #
+#   |~+~===~+==<>==+~===~+~===~+==<>==+~===~+~===~+==<>==+~===~+~|   #
+#   |  <<  <<  <>  <|   Advent  of  Code  2022   |>  <>  >>  >>  |   #
+#   |~+~===~+==<>==+~===~+~===~+==<>==+~===~+~===~+==<>==+~===~+~|   #
+#   | + ::|  Day  1  |:: + ::|  Jesse Williams  ∕  xram64  |:: + |   #
+#   |~+~===~+==<>==+~===~+~===~+==<>==+~===~+~===~+==<>==+~===~+~|   #
+#   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   #
+#             __.-----------.___________________________             #
+#            |  |  Answers  |   Part 1: 69310           |            #
+#            |  `-----------'   Part 2: 206104          |            #
+#            `------------------------------------------'            #
+#│░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░│#
+
 
 from collections import namedtuple
 Count = namedtuple('Count', ['elf', 'value'])
 
-## Part 1
+#╷----------.
+#│  Part 1  │
+#╵----------'
 with open('day01_input.txt') as f:
     lines = f.readlines()
 
@@ -27,7 +39,8 @@ counts.sort(key=lambda x: x.value, reverse=True)
 count_max = counts[0]
 print(f"[Part 1] Elf #{count_max.elf+1:03d} is carrying the most calories, with {count_max.value} total calories.")
 
-
-## Part 2
+#╷----------.
+#│  Part 2  │
+#╵----------'
 print(f"[Part 2] Elves #{counts[0].elf+1:03d}, #{counts[1].elf+1:03d}, and #{counts[2].elf+1:03d} carry the most calories",
       f"with {counts[0].value+counts[1].value+counts[2].value} total calories between them.")
